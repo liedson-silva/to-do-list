c29c64f92a5593956b38c25fd586b2c24f251427
liedson-silva <liedsonsilva987@gmail.com>
2025-08-19 19:47:47 -0300

    Subindo para o vercel

--- frontEnd/src/services/axios.js
+++ frontEnd/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://to-do-list-nu-lovat-66.vercel.app'
+    baseURL: 'https://to-do-list-nu-lovat-66.vercel.app'
 })
 
 export default api
